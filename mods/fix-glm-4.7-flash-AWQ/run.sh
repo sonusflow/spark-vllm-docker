c23aff91d3232a176a56f80f48d5ea66cdf2aa39
Eugene Rakhmatulin <eugr@eugr.com>
2026-02-16 09:23:10 -0800

    Temporary fix for #38

--- mods/fix-glm-4.7-flash-AWQ/run.sh
+++ mods/fix-glm-4.7-flash-AWQ/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 set -e
+echo "--- Applying GLM 4.7 AWQ speed patch..."
 patch -p1 -d / < glm47_flash.patch
+echo "=== OK"
+echo "--- Applying vLLM crash patch..."
+patch -p1 -d /usr/local/lib/python3.12/dist-packages < glm47_vllm_bug.patch || echo "=== Patch is not applicable, skipping"
+echo "=== OK"
